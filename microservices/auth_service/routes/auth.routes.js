04f8413677744b587c0c7ac28bb18cb6882dd70b
Zhephyr <Raulsalas703hotmsilcom@gmail.com>
2026-08-16 22:13:12 -0600

    Agregar middleware JWT y ruta protegida

--- microservices/auth_service/routes/auth.routes.js
+++ microservices/auth_service/routes/auth.routes.js
@@ -5,10 +5,21 @@ const {
   login
 } = require('../controllers/auth.controller');
 
+const verifyToken = require('../middleware/auth.middleware');
+
 const router = express.Router();
 
 router.post('/register', register);
 
 router.post('/login', login);
+
+
+// Ruta protegida
+router.get('/verify', verifyToken, (req, res) => {
+  res.status(200).json({
+    message: 'Token válido',
+    user: req.user
+  });
+});
 
 module.exports = router;
